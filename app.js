b5ea0ed5b7c171e01d92e8af1d8c23838218fbef
Adam Pizzo <50724722+adampizzo@users.noreply.github.com>
2023-07-09 14:20:37 -0400

    setup generator with imports instead of requires. Left comments in for reference.

--- app.js
+++ app.js
@@ -1,41 +1,60 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// var createError = require("http-errors");
+// var express = require("express");
+// var path = require("path");
+// var cookieParser = require("cookie-parser");
+// var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var indexRouter = require("./routes/index");
+// var usersRouter = require("./routes/users");
 
-var app = express();
+import createHttpError from "http-errors";
+import express from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+
+import indexRouter from "./routes/indexRouter.js";
+import userRouter from "./routes/userRouter.js";
+import campsiteRouter from "./routes/campsiteRouter.js";
+import partnerRouter from "./routes/partnerRouter.js";
+import promotionRouter from "./routes/promotionRouter.js";
+
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", userRouter);
+app.use("/campsites", campsiteRouter);
+app.use("/promotions", promotionRouter);
+app.use("/partners", partnerRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createHttpError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
 });
 
-module.exports = app;
+export default app;
